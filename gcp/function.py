7b47d29be4907be71444b3561464b41df540d962
FutureGoose <evilgoosecode@gmail.com>
2024-08-21 10:45:10 +0200

    update: suffix

---
+++ gcp/function.py
@@ -0,0 +1,11 @@
+import datetime
+import functions_framework
+from flask import jsonify
+
+@functions_framework.http
+def get_yesterday_date(request):
+    today = datetime.datetime.now()
+    yesterday = today - datetime.timedelta(days=1)
+    formatted_yesterday = yesterday.strftime("%Y-%m-%d")
+    return jsonify({'date': formatted_yesterday})
+	
