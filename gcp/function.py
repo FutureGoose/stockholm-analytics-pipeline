b9945bdc3d7dabf31eac0f4c6664db4eb3fc7633
FutureGoose <evilgoosecode@gmail.com>
2024-08-23 11:04:55 +0200

    fix: func now correctly returns dict

--- gcp/function.py
+++ gcp/function.py
@@ -7,4 +7,4 @@ def get_yesterday_date(request) -> str:
     today = datetime.datetime.now()
     yesterday = today - datetime.timedelta(days=1)
     formatted_yesterday = yesterday.strftime("%Y-%m-%d")
-    return formatted_yesterday
+    return jsonify({'date': formatted_yesterday})
