b4ce642a1a93d7f09059d98e304a2af2298cb68f
speedan <malmborg_91@hotmail.com>
2024-08-26 16:01:35 +0200

    trying get instead of post

--- ml-model/model_service.py
+++ ml-model/model_service.py
@@ -1,48 +1,39 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Query
+from google.cloud import bigquery
 from pydantic import BaseModel
 import joblib
-import pandas as pd
-import numpy as np
 
 app = FastAPI()
 
-# Load the pre-trained model
-model = joblib.load("weather_forecasting_model_stockholm_xgb.pkl")
+# Load the model
+model = joblib.load('model.joblib')
 
-# Define the input data schema
-class InputData(BaseModel):
-    hour: int
-    month: int
-    temp: float
-    humidity: float
-    pressure: float
-    temp_lag_1: float
-    temp_lag_3: float
+@app.get("/predict")
+async def predict(
+    hour: int,
+    month: int,
+    temp: float,
+    humidity: float,
+    pressure: float,
+    temp_lag_1: float,
+    temp_lag_3: float):
 
-@app.get("/")
-async def read_root():
-    return {"message": "Welcome to the ML-Model API"}
+    # Fetch data from BigQuery
+    client = bigquery.Client()
+    query = f"""
+    SELECT * FROM `team-god.weather_data.clean_weatherapp`
+    """
+    df = client.query(query).to_dataframe()
 
-@app.post("/predict")
-def predict(data: InputData):
-    try:
-        # Convert input data to DataFrame using model_dump() method
-        input_df = pd.DataFrame([data.model_dump()])
+    # Assuming your model requires similar data format
+    features = df[['hour', 'month', 'temp', 'humidity', 'pressure', 'temp_lag_1',
+    'temp_lag_3']].iloc[0]
 
-        # Perform prediction
-        prediction = model.predict(input_df)
-        
-        # Handle prediction result
-        if hasattr(prediction, 'tolist'):
-            prediction = prediction.tolist()
-        elif isinstance(prediction, (np.float32, np.float64)):
-            prediction = float(prediction)
+    # Make prediction
+    prediction = model.predict([features])[0]
 
-        # Return the prediction
-        return {"prediction": prediction[0]}
-    
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Prediction error: {str(e)}")
+    return {"max_temperature_prediction": prediction}
+
 
 # To run the FastAPI server:
 # uvicorn model_service:app --reload
