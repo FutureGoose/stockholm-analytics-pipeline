803e283d7c693e0849a30511c52f8ae71f4436e9
danhag123 <dhaglund1990@gmail.com>
2024-09-03 13:11:10 +0200

    removed timestamp temporarily

--- pipes/search-trends-api-raw/src/main.py
+++ pipes/search-trends-api-raw/src/main.py
@@ -31,7 +31,7 @@ def fetch_trends_data(kw_list: List[str]) -> pd.DataFrame:
     data = pytrends.interest_over_time()
     
     # Add ingestion timestamp
-    data['ingestion_timestamp'] = pendulum.now().to_datetime_string()
+    # data['ingestion_timestamp'] = pendulum.now().to_datetime_string()
 
     # Rename columns to replace spaces with underscores
     data.columns = [col.replace(' ', '_') for col in data.columns]
