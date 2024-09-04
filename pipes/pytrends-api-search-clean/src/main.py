036e7780640c367b4ce77e06412b3f67931974eb
danhag123 <dhaglund1990@gmail.com>
2024-09-04 14:13:06 +0200

    replace 'å' and 'ä' with 'a' and 'ö' with 'o' in column names

--- pipes/pytrends-api-search-clean/src/main.py
+++ pipes/pytrends-api-search-clean/src/main.py
@@ -34,7 +34,9 @@ def fetch_trends_data(kw_list: List[str]) -> pd.DataFrame:
     data['ingestion_timestamp'] = pd.to_datetime(pendulum.now().to_datetime_string())
 
     # Rename columns to replace spaces with underscores
-    data.columns = [col.replace(' ', '_') for col in data.columns]
+    data.columns = [col.replace('å', 'a').replace('ä', 'a').replace('ö', 'o').replace(' ', '_') for col in data.columns]
+
+    # Rename columns to replace 
     
     return data
 
