5f4c85517465b3581b4d8e8f2ce15ccea2d94eb8
FutureGoose <evilgoosecode@gmail.com>
2024-09-10 18:22:58 +0200

    add timeout to pytrends, comment out time sleep in main and add raise for try except in main

--- pipes/pytrends-api-search-clean/src/main.py
+++ pipes/pytrends-api-search-clean/src/main.py
@@ -35,10 +35,12 @@ def fetch_trends_data(kw_list: List[str]) -> pd.DataFrame:
     """
     max_retries = 19
     backoff_factor = 60
+    timeout = 10
+
     for attempt in range(max_retries):
         try:
             # Payload with settings "all categories", "past 3 months", "Stockholm" and "web searches"
-            pytrends.build_payload(kw_list, cat=0, timeframe='today 3-m', geo='SE-AB', gprop='')
+            pytrends.build_payload(kw_list, cat=0, timeframe='today 3-m', geo='SE-AB', gprop='', timeout=timeout)
             data = pytrends.interest_over_time()
             
             # Add ingestion timestamp as a datetime object
@@ -75,8 +77,10 @@ def main():
         try:
             send_to_bigquery(trends_data, f"{idx+1}")
         except Exception as e:
-            print(f"Error sending data to BigQuery: {e}")
+            error_message =f"Error sending data to BigQuery: {e}"
+            print(error_message)
+            raise HTTPException(status_code=500, detail=error_message)
         
-        time.sleep(60)
+        #time.sleep(60)
             
     return {"status_code": 200}
